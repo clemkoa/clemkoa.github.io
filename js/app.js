20f262ac5b4743f6c718f49334e08105f10dd360
Clemkoa <Clemkoa>
2015-06-18 10:00:39 +0200

    Pages protégée par login

--- js/app.js
+++ js/app.js
@@ -1,22 +1,50 @@
 // app.js
 var routerApp = angular.module('routerApp', ['ui.router']);
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
-    
-    $urlRouterProvider.otherwise('/home');
-    
-    $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-            templateUrl: 'partials/home.html'
-        })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            url: '/about',
-            templateUrl: 'partials/about.html'     
-        });
-        
+routerApp.config(function($stateProvider, $urlRouterProvider) {  
+  $urlRouterProvider.otherwise('/login');
+  
+  $stateProvider
+      
+  // HOME STATES AND NESTED VIEWS ========================================
+  .state('home', {
+      url: '/home',
+      templateUrl: 'partials/home.html',
+      data: {
+        requireLogin: true
+      }
+  })
+  
+  // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+  .state('about', {
+      url: '/about',
+      templateUrl: 'partials/about.html',
+      data: {
+        requireLogin: true
+      }
+  })
+
+  .state('login', {
+      url: '/login',
+      templateUrl: 'partials/login.html',
+      data: {
+        requireLogin: false
+      }
+  });
+});
+
+
+routerApp.run(function ($rootScope) {
+  $rootScope.currentUser = 'undefined';
+  
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+    var requireLogin = toState.data.requireLogin;
+
+    if (requireLogin && $rootScope.currentUser == 'undefined') {
+      //empêcher d'accéder à la page sans être identitier
+      console.log($rootScope.currentUser);
+      event.preventDefault();
+    }
+  });
+
 });
--- test.coffee
+++ test.coffee
@@ -1,9 +1,9 @@
 http: require "http"
  
-http.createServer( (req, res) ->
-  res.sendHeader 200, {"Content-Type": "text/plain"}
+http.createServer (req, res) ->
+  res.sendHeader 200, "Content-Type": "text/plain"
   res.sendBody "Hello, World!"
   res.finish()
-).listen 8000
+.listen 8000
  
 puts "Server running at http://127.0.0.1:8000/"
